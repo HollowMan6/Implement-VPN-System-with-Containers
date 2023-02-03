018080c6f4e182ccc2dfb681de6cc8743fc3242c
Hollow Man <hollowman@opensuse.org>
2023-02-03 16:48:54 +0200

    Change network address
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

---
+++ src/gen_certs.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+mkdir private certs crls cacerts reqs
+
+# CA
+pki --gen --type ecdsa --outform pem > caKey.pem
+pki --self --ca --lifetime 3652 --in caKey.pem --digest sha512 \
+           --dn "C=FI, O=CSE4300, CN=CSE4300 Root CA" \
+           --outform pem > cacerts/caCert.pem
+
+# Intermediate CA
+pki --gen --type ecdsa --outform pem > intCaKey.pem
+pki --req --type priv --in intCaKey.pem \
+          --dn "C=FI, O=CSE4300, CN=CSE4300 INT CA" \
+          --outform pem > reqs/intCaReq.pem
+pki --issue --cacert cacerts/caCert.pem --cakey caKey.pem --digest sha512 \
+            --type pkcs10 --in reqs/intCaReq.pem --lifetime 1826 \
+            --outform pem --ca --pathlen 0 > cacerts/intCaCert.pem
+
+# Site a
+pki --gen --type ecdsa --outform pem > private/siteAKey.pem
+pki --req --type priv --in private/siteAKey.pem \
+          --dn "C=FI, O=CSE4300, CN=CSE4300 Site A 172.6.6.6" \
+          --san 172.6.6.6 --outform pem > reqs/siteAReq.pem
+pki --issue --cacert cacerts/intCaCert.pem --cakey intCaKey.pem \
+            --type pkcs10 --in reqs/siteAReq.pem --lifetime 913 \
+            --outform pem --flag clientAuth --digest sha512 > certs/siteACert.pem
+
+# Site b
+pki --gen --type ecdsa --outform pem > private/siteBKey.pem
+pki --req --type priv --in private/siteBKey.pem \
+          --dn "C=FI, O=CSE4300, CN=CSE4300 Site B 172.8.8.8" \
+          --san 172.8.8.8 --outform pem > reqs/siteBReq.pem
+pki --issue --cacert cacerts/intCaCert.pem --cakey intCaKey.pem \
+            --type pkcs10 --in reqs/siteBReq.pem --lifetime 913 \
+            --outform pem --flag clientAuth --digest sha512 > certs/siteBCert.pem
+
+# Cloud
+pki --gen --type ecdsa --outform pem > private/cloudKey.pem
+pki --req --type priv --in private/cloudKey.pem \
+          --dn "C=FI, O=CSE4300, CN=CSE4300 Cloud 172.7.7.7" \
+          --san 172.7.7.7 --outform pem > reqs/cloudReq.pem
+pki --issue --cacert cacerts/intCaCert.pem --cakey intCaKey.pem \
+            --type pkcs10 --in reqs/cloudReq.pem --lifetime 913 \
+            --outform pem --flag serverAuth --digest sha512 > certs/cloudCert.pem
+
+# CRL
+pki --signcrl --cacert cacerts/caCert.pem --cakey caKey.pem \
+              --lifetime 365 > crls/cse4300Ca.crl
+pki --signcrl --cacert cacerts/intCaCert.pem --cakey intCaKey.pem \
+              --lifetime 365 > crls/cse4300IntCa.crl
--- src/site-to-site/router.sh
+++ src/site-to-site/router.sh
@@ -5,7 +5,7 @@
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 ## Route 172.30.0.0/16 all via gateway-s
-route add -net 172.48.48.48 netmask 255.255.255.240 gw 172.30.30.30 eth3
+route add -net 172.48.48.48 netmask 255.255.255.240 gw 172.7.7.7 eth3
 
 ## Save the iptables rules
 iptables-save > /etc/iptables/rules.v4
